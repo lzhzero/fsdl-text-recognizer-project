22ec49be6f67fdb95a7a7456aafa946aeb395b7b
zliu <zliu@local>
2018-08-03 22:12:32 +0000

    my lab1 work

--- lab2/text_recognizer/networks/lenet.py
+++ lab2/text_recognizer/networks/lenet.py
@@ -9,7 +9,17 @@ def lenet(input_shape: Tuple[int, ...], output_shape: Tuple[int, ...]) -> Model:
     num_classes = output_shape[0]
 
     ##### Your code below (Lab 2)
-
+    model = Sequential()
+    model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape=input_shape))
+    input_shape = (input_shape[0], input_shape[1], 1)
+    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape = input_shape))
+    model.add(MaxPooling2D(poolsize(2,2)))
+    model.add(Conv2D(32, kernel_size=(5,5), activation='relu'))
+    model.add(MaxPooling2D(poolsize(2,2)))
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(Dropout(0.1))
+    model.add(Dense(num_classes))
     ##### Your code above (Lab 2)
 
     return model
